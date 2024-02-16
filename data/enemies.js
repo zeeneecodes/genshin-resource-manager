38bb03c9cbdcece889dff5f8d9a2ab3febb289e3
zeenee <cabrerarizzi@gmail.com>
2024-02-16 20:18:55 +0800

    Add Easy Enemies data

---
+++ data/enemies.js
@@ -0,0 +1,15 @@
+const easyEnemyDetails = {
+	'Clockwork Meka': ['Meshing Gear','Mechanical Spur Gear','Artificed Dynamic Gear'],
+	'Eremites': ['Faded Red Satin','Trimmed Red Silk','Rich Red Brocade'],
+	'Fatui Insignia': ["Recruit's Inisignia","Sergeant's Inisgnia","Lieutenant's Insignia"],
+	'Fungi':['Fungal Spores','Luminescent Pollen','Crystalline Cyst Dust'],
+	'Handguard':['Old Handguard','Kageuchi Handguard','Famed Handguard'],
+	'Hilichurl Arrow':['Firm Arrowhead','Sharp Arrowhead','Weathered Arrowhead'],
+	'Hilichurl Mask':['Damaged Mask','Stained Mask','Ominous Mask'],
+	'Samachurl Scroll':['Divining Scroll','Sealed Scroll','Forbidden Curse Scroll'],
+	'Sea Creatures':['Transoceanic Pearl','Transoceanic Chunk','Transoceanic Crystal'],
+	'Slime':['Slime Condensate','Slime Secretions','Slime Concentrate'],
+	'Specters':['Spectral Husk','Spectral Heart','Spectral Nucleus'],
+	'Treasure Hoarder':['Treasure Hoarder Insignia','Silver Raven Insignia','Golden Raven Inisgnia'],
+	'Whopperflower':['Whopperflower Nectar','Shimmering Nectar','Energy Nectar']
+};

1d93f86aca1bc65d1b11d4cabd298903bebd364f
zeenee <cabrerarizzi@gmail.com>
2024-02-19 19:25:30 +0800

    initial commit

---
+++ data/ascension.js
@@ -0,0 +1,50 @@
+const characterAscenscionInfo = {
+	ascenscion1: {
+		stones:[1,0,0,0],
+		dailyBoss:0,
+		regionSpecialty:3,
+		mobDrop:[3,0,0],
+		mobDropRating:1,
+		mora:20000
+	},
+	ascenscion2: {
+		stones:[0,3,0,0],
+		dailyBoss:2,
+		regionSpecialty:10,
+		mobDrop:[15,0,0],
+		mobDropRating:1,
+		mora:20000
+	},
+	ascenscion3: {
+		stones:[0,6,0,0],
+		dailyBoss:4,
+		regionSpecialty:20,
+		mobDrop:[0,12,0],
+		mobDropRating:2,
+		mora:40000
+	},
+	ascenscion4: {
+		stones:[0,0,3,0],
+		dailyBoss:8,
+		regionSpecialty:30,
+		mobDrop:[0,18,0],
+		mobDropRating:2,
+		mora:80000
+	},
+	ascenscion5: {
+		stones:[0,0,6,0],
+		dailyBoss:12,
+		regionSpecialty:45,
+		mobDrop:[0,0,12],
+		mobDropRating:3,
+		mora:100000
+	},
+	ascenscion6: {
+		stones:[0,0,0,6],
+		dailyBoss:20,
+		regionSpecialty:60,
+		mobDrop:[0,0,24],
+		mobDropRating:3,
+		mora:120000
+	}
+};
